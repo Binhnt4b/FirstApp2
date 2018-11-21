2c4d9df730dca17b5d58552fbb23e73718725ddf
Binhnt4b <45237000+Binhnt4b@users.noreply.github.com>
2018-11-21 22:26:38 +0700

    Add files
    Add files

---
+++ FirstMobileApp/FirstMobileApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace FirstMobileApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            message.Text = "Loading item...";
+
+            //MobileServiceClient client = new MobileServiceClient("http://localhost:49366");
+            //var items = await client.GetTable<TodoItem>().ReadAsync();
+            //var item = items.First();
+
+            message.Text = "Test Android";
+        }
+    }
+}
